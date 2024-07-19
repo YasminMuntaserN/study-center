1dbaa577aace521091768e6428615ad30d3b87c3
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-07-19 14:44:09 +0300

    feat: add GradeLevel into the Businees Layer

---
+++ StudyCenter_DataAccessLayer/clsGradeLevelData.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.Data.SqlClient;
+using StudyCenter_DataAccessLayer.Global_classes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCenter_DataAccessLayer
+{
+    public class clsGradeLevelData
+    {
+        public static bool GetInfoByID(int? gradeLevelID, ref string gradeName)
+        {
+            bool isFound = false;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("SP_GetGradeLevelInfoByID", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@GradeLevelID", (object)gradeLevelID ?? DBNull.Value);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                // The record was found
+                                isFound = true;
+
+                                gradeName = (string)reader["GradeName"];
+                            }
+                            else
+                            {
+                                // The record was not found
+                                isFound = false;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                isFound = false;
+                clsErrorLogger.LogError(ex);
+            }
+
+            return isFound;
+        }
+
+        public static byte? Add(string gradeName)
+        {
+            // This function will return the new person id if succeeded and null if not
+            byte? gradeLevelID = null;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("SP_AddNewGradeLevel", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@GradeName", gradeName);
+
+                        SqlParameter outputIdParam = new SqlParameter("@NewGradeLevelID", SqlDbType.Int)
+                        {
+                            Direction = ParameterDirection.Output
+                        };
+                        command.Parameters.Add(outputIdParam);
+
+                        command.ExecuteNonQuery();
+
+                        gradeLevelID = (byte?)(int)outputIdParam.Value;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                clsErrorLogger.LogError(ex);
+            }
+
+            return gradeLevelID;
+        }
+
+        public static bool Update(int gradeLevelID, string gradeName)
+        {
+            int rowAffected = 0;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("SP_UpdateGradeLevel", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@GradeLevelID", gradeLevelID);
+                        command.Parameters.AddWithValue("@GradeName", gradeName);
+
+                        rowAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                clsErrorLogger.LogError(ex);
+            }
+
+            return (rowAffected > 0);
+        }
+
+        public static bool Delete(int? gradeLevelID)
+            => clsDataAccessHelper.Delete("SP_DeleteGradeLevel", "GradeLevelID", gradeLevelID);
+
+        public static bool Exists(int? gradeLevelID)
+            => clsDataAccessHelper.Exists("SP_DoesGradeLevelExistByGradeLevelID", "GradeLevelID", gradeLevelID);
+
+        public static bool Exists(string gradeName)
+            => clsDataAccessHelper.Exists("SP_DoesGradeLevelExistByGradeLevelName", "GradeLevelName", gradeName);
+
+        public static DataTable All()
+            => clsDataAccessHelper.All("SP_GetAllGradeLevels");
+
+        public static string GetGradeLevelName(int? gradeLevelID)
+        {
+            // This function will return the new person id if succeeded and null if not
+            string gradeName = null;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("SP_GetGradeLevelName", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@GradeLevelID", gradeLevelID);
+
+                        SqlParameter outputIdParam = new SqlParameter("@GradeName", SqlDbType.NVarChar, 50)
+                        {
+                            Direction = ParameterDirection.Output
+                        };
+                        command.Parameters.Add(outputIdParam);
+
+                        command.ExecuteNonQuery();
+
+                        gradeName = outputIdParam.Value.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                clsErrorLogger.LogError(ex);
+            }
+
+            return gradeName;
+        }
+    }
+}
---
+++ studyCenter_BusineesLayer/clsGradeLevel.cs
@@ -0,0 +1,92 @@
+﻿using StudyCenter_DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyCenter_BusineesLayer
+{
+    public class clsGradeLevel
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int? GradeLevelID { get; set; }
+        public string GradeName { get; set; }
+
+        public clsGradeLevel()
+        {
+            GradeLevelID = null;
+            GradeName = string.Empty;
+
+            Mode = enMode.AddNew;
+        }
+
+        private clsGradeLevel(int? gradeLevelID, string gradeName)
+        {
+            GradeLevelID = gradeLevelID;
+            GradeName = gradeName;
+
+            Mode = enMode.Update;
+        }
+
+        private bool _Add()
+        {
+            GradeLevelID = clsGradeLevelData.Add(GradeName);
+
+            return (GradeLevelID.HasValue);
+        }
+
+        private bool _Update()
+        {
+            return clsGradeLevelData.Update(GradeLevelID.Value, GradeName);
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_Add())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+                    return _Update();
+            }
+
+            return false;
+        }
+
+        public static clsGradeLevel Find(int gradeLevelID)
+        {
+            string gradeName = string.Empty;
+
+            bool isFound = clsGradeLevelData.GetInfoByID(gradeLevelID, ref gradeName);
+
+            return (isFound) ? (new clsGradeLevel(gradeLevelID, gradeName)) : null;
+        }
+
+        public static bool Delete(byte? gradeLevelID)
+            => clsGradeLevelData.Delete(gradeLevelID);
+
+        public static bool Exists(byte? gradeLevelID)
+            => clsGradeLevelData.Exists(gradeLevelID);
+
+        public static bool Exists(string gradeName)
+            => clsGradeLevelData.Exists(gradeName);
+
+        public static DataTable All() => clsGradeLevelData.All();
+
+        public static string GetGradeLevelName(byte? gradeLevelID)
+            => clsGradeLevelData.GetGradeLevelName(gradeLevelID);
+    }
+}
